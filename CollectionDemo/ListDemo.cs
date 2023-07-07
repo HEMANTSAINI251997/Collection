7fc89c9595c263a728a6b8184f79009d949f22f3
HEMANTSAINI251997 <hemantsaini2525@gmail.com>
2023-07-07 09:03:21 +0530

    [HEMANT]Add: All function of List

---
+++ CollectionDemo/ListDemo.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionDemo
+{
+    public class ListDemo
+    {
+        List<int> list = new List<int>();
+        public void Adding()
+        {
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            list.Add(4);
+            list.Add(5);
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("Count of LIST =" + list.Count);
+            Console.WriteLine("List Capacity =" + list.Capacity);
+            list.Add(21);
+            list.Add(30);
+            Console.WriteLine("List Capacity =" + list.Capacity);
+            Console.WriteLine("Find element =" + list.Contains(3));
+            Console.WriteLine("Removed element =" + list.Remove(3));
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("************");
+
+            Console.WriteLine("Remove Element by Index position");
+            list.RemoveAt(4);
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("************");
+            Console.WriteLine("Sorting the List");
+
+
+            list.Sort();
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("************");
+            Console.WriteLine("Reverse the List");
+
+            list.Reverse();
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("************");
+
+
+            list.Clear();
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("************");
+
+
+        }
+    }
+}
---
+++ CollectionDemo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionDemo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ListDemo listdemo = new ListDemo();
+            listdemo.Adding();
+        }
+    }
+}
